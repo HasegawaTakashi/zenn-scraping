7765336d1d1a2d740c0ebaee2424c3dc671e9362
HasegawaTakashi <euphonium813@gmail.com>
2022-11-07 12:18:30 +0900

    [edit] zenn scraping

--- src/python/main.py
+++ src/python/main.py
@@ -1,22 +1,62 @@
-import requests
-
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+from time import sleep
 
 # 変数urlに、Zenn新着ページのURLを入れる
-url = 'https://zenn.dev/articles?page={1}'
+# url = 'https://zenn.dev/articles?page={}'
+url = 'https://zenn.dev/topics/python'
 
-# urlへのアクセス結果を変数rに格納
-r = requests.get(url)
+# 変数d_listに空のリストを作成する
+d_list = []
+#ページ番号i
+i=0
+# アクセスするためのURLをtarget_urlに格納する 
+while True:
+    # i +=1
+    # target_url = url.format(i)
+    target_url = url
+    # target_urlへのアクセス結果を、変数rに格納
+    r = requests.get(target_url)
 
-# 取得結果を解析してsoupに格納
-soup = BeautifulSoup(r.text)
+    if r.status_code == 200:
+        print('d_listの大きさ：', len(d_list))
+        # print()してtarget_urlを確認する
+        print(target_url)
 
-# ArticleList_itemContainer__xlBMcクラスを持ったdivタグをすべて取得して、変数contentsに格納
-contents = soup.find_all('div', class_='ArticleList_itemContainer__xlBMc')
+        # 取得結果を解析してsoupに格納
+        soup = BeautifulSoup(r.text)
+        # ArticleList_itemContainer__xlBMcクラスを持ったdivタグをすべて取得して、変数contentsに格納
+        contents = soup.find_all('div', class_='ArticleList_itemContainer__xlBMc')
+        #1秒ウェイトを入れる
+        sleep(1)
 
-print(contents)
+        for content in contents:
+            #URLlink
+            link = 'https://zenn.dev' + content.a.get('href')
+            #タイトル
+            title= content.find('h2').text
+            #著者
+            author = content.find('div', class_='ArticleList_userName__GWXDx').text
+            #時間
+            time  = content.time.get('datetime')
+            # 変数dに、これまで取得した項目を格納する
+            d = {
+                'title': title,
+                'author': author,
+                'link': link,
+                 'time': time
+            }
+            d_list.append(d)
+    else:
+        break 
 
+
+# 変数d_listを使って、データフレームを作成する
+# df = pd.DataFrame(d_list)
+
+# to_csv()を使って、データフレームをCSV出力する
+# df.to_csv('ファイルの保存場所/ファイル名.csv', index=None, encoding='utf-8-sig')
 
 #r = requests.get('https://nikkei225jp.com/chart/')
 #text = r.text
@@ -29,4 +69,3 @@ print(contents)
 #print ('日経株価は ',nikkei, '円です')
 #print ('ダウ平均株価は', dau, '円です')
 #print ('為替ドルは', kawase,'円です')
-
