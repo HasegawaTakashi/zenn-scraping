b47782704a7cef93add93ce807c094959e8a45c2
HasegawaTakashi <euphonium813@gmail.com>
2022-11-08 17:25:59 +0900

    [edit] in the process of being edited

--- src/python/main.py
+++ src/python/main.py
@@ -5,46 +5,7 @@ from time import sleep
 import datetime
 import mysql.connector
 
-class connect:
-    sleep(15)
-    cnx = mysql.connector.connect(
-        host = '192.168.2.2',
-        port = '3306',
-        user = 'root',
-        password = 'root',
-        database = 'zenn',
-    )
-    cursor = cnx.cursor()
-
-    query1 = "CREATE DATABASE IF NOT EXISTS zenn"
-    cursor.execute(query1)
-    print('finished create database')
-
-    # create new table
-    query2 = "CREATE TABLE IF NOT EXISTS article ( \
-    id int not null auto_increment primary key, \
-    title varchar(100), \
-    author varchar(50), \
-    link varchar(200), \
-    created_at datetime not null default current_timestamp)"
-    cursor.execute(query2)
-    print('finished create table')
-
-    # test insert
-    query3 = "INSERT INTO zenn.article (title, author, link) values( \
-    'Docker連携リバースプロキシで複数コンテナへのアクセスを簡単にする[Traefik]', \
-    'ArkBig', \
-    'https://zenn.dev/arkbig/articles/traefik_e615f702955ad113e551e2a22f7ff65f10ba1b0bbc' \
-    '2022-11-08' \
-    );"
-    cursor.execute(query3)
-    cnx.commit()
-    print('finished insert test data')
-
-test = connect()
-print('finished')
-
-"""
+sleep(15)
 class Scraping:
 
     def scraping(string, url):
@@ -83,9 +44,53 @@ class Scraping:
         df = pd.DataFrame(d_list)
 
         # to_csv()を使って、データフレームをCSV出力する
-        df.to_csv(f'./csv_files/{string}_' + str(datetime.date.today()) + '.csv', index=None, encoding='utf-8-sig')
+        # df.to_csv(f'./csv_files/{string}_' + str(datetime.date.today()) + '.csv', index=None, encoding='utf-8-sig')
 
-        print(string, 'is success')
+        # print(string, 'is success')
+        # print(d_list)
+
+        cnx = mysql.connector.connect(
+            host = '192.168.2.2',
+            port = '3306',
+            user = 'root',
+            password = 'root',
+            database = 'zenn',
+        )
+        cursor = cnx.cursor()
+
+        query1 = "CREATE DATABASE IF NOT EXISTS zenn"
+        cursor.execute(query1)
+        print('finished create database')
+
+        # create new table
+        query2 = "CREATE TABLE IF NOT EXISTS article ( \
+        id int not null auto_increment primary key, \
+        title varchar(100), \
+        author varchar(50), \
+        link varchar(200), \
+        created_at datetime not null default current_timestamp)"
+        cursor.execute(query2)
+        print('finished create table')
+
+        # test insert
+        # query3 = "INSERT INTO zenn.article (title, author, link) values( \
+        # 'Docker連携リバースプロキシで複数コンテナへのアクセスを簡単にする[Traefik]', \
+        # 'ArkBig', \
+        # 'https://zenn.dev/arkbig/articles/traefik_e615f702955ad113e551e2a22f7ff65f10ba1b0bbc' \
+        # '2022-11-08' \
+        # );"
+        # cursor.execute(query3)
+        # cnx.commit()
+        # print('finished insert test data')
+
+        title = d_list[0]['title']
+        author = d_list[0]['author']
+        link = d_list[0]['link']
+
+        query4 = ("INSERT INTO zenn.article(title, author, link) VALUES (%s)", title)
+        cursor.execute(query4)
+        cnx.commit()
+        print('finished insert variable data')
 
 
 def save_to_csv():
@@ -111,16 +116,5 @@ def save_to_csv():
         num += 1
 
 save_to_csv()
+print('finished all')
 
-#r = requests.get('https://nikkei225jp.com/chart/')
-#text = r.text
-#date = text.split('<div class=wtimeT>')[1].split('</div>')[0]
-#nikkei = text.split('<div class=if_cur>')[1].split('</div>')[0].replace(',','')
-#dau = text.split('<div class=if_cur>')[2].split('</div>')[0].replace(',','')
-#kawase = text.split('<div class=if_cur>')[3].split('</div>')[0].replace(',','')
-
-#print('今日は',date,'です')
-#print ('日経株価は ',nikkei, '円です')
-#print ('ダウ平均株価は', dau, '円です')
-#print ('為替ドルは', kawase,'円です')
-"""
