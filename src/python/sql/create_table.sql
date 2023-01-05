8a9fb287f868429241ee62a315033658a8088135
HasegawaTakashi <89558052+HasegawaTakashi@users.noreply.github.com>
2023-01-06 00:23:44 +0900

    Delete src/python/sql directory

--- src/python/sql/create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS {table_name} (
-id int not null auto_increment primary key,
-title varchar(100),
-author varchar(50),
-link varchar(200),
-created_at datetime not null default current_timestamp)
--- src/python/sql/init.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM zenn.{table_name}
